6fe2998b802e557973e00a740dafef38c7e4c589
Oleks <{ID}+{username}@users.noreply.github.com>
2023-02-14 17:49:14 -0500

    add inverse matrix

--- src/main/java/com/csdisciple/hill_cipher/HillCipherService.java
+++ src/main/java/com/csdisciple/hill_cipher/HillCipherService.java
@@ -7,9 +7,9 @@ import java.util.Locale;
 import java.util.Random;
 
 /*
- * Step one: convert the message to a 1D array
+ * Step one: convert the message to a 2D array
  * Step two: convert key to a 2D array
- * Multiply 2Dx1D = result1DArray mod 26 (%26)
+ * Multiply 2Dx2D = result2DArray mod 26 (%26)
  * Then convert number array to letters
  * Output encrypted result as String
  * */
@@ -18,18 +18,170 @@ import java.util.Random;
  * To Decrypt
  * Take an inverse of the key matrix and then mod 26
  * multiply the inverse matrix by the encrypted message
- * Then mod 26 the one dimensional matrix to get decrypted message
+ * Then mod 26 the matrix to get decrypted message
  *
  * */
 @Service
 public class HillCipherService {
+    String key, encryptedMessage;
+    int[][] keyMatrix, messageMatrix, encryptedKeyMatrix;
+
+    public static int[][] invert(int a[][]) {
+
+        int n = a.length;
+
+        int x[][] = new int[n][n];
+
+        int b[][] = new int[n][n];
+
+        int index[] = new int[n];
+
+        for (int i = 0; i < n; ++i)
+
+            b[i][i] = 1;
+
+
+        // Transform the matrix into an upper triangle
+
+        gaussian(a, index);
+
+
+        // Update the matrix b[i][j] with the ratios stored
+
+        for (int i = 0; i < n - 1; ++i)
+
+            for (int j = i + 1; j < n; ++j)
+
+                for (int k = 0; k < n; ++k)
+
+                    b[index[j]][k]
+
+                            -= a[index[j]][i] * b[index[i]][k];
+
+
+        // Perform backward substitutions
+
+        for (int i = 0; i < n; ++i) {
+
+            x[n - 1][i] = b[index[n - 1]][i] / a[index[n - 1]][n - 1];
+
+            for (int j = n - 2; j >= 0; --j) {
+
+                x[j][i] = b[index[j]][i];
+
+                for (int k = j + 1; k < n; ++k) {
+
+                    x[j][i] -= a[index[j]][k] * x[k][i];
+
+                }
+
+                x[j][i] /= a[index[j]][j];
+
+            }
+
+        }
+
+        return x;
+
+    }
+
+    public static void gaussian(int a[][], int index[]) {
+
+        int n = index.length;
+
+        int c[] = new int[n];
+
+
+        // Initialize the index
+
+        for (int i = 0; i < n; ++i)
+
+            index[i] = i;
+
+
+        // Find the rescaling factors, one from each row
+
+        for (int i = 0; i < n; ++i) {
+
+            int c1 = 0;
+
+            for (int j = 0; j < n; ++j) {
+
+                int c0 = Math.abs(a[i][j]);
+
+                if (c0 > c1) c1 = c0;
+
+            }
+
+            c[i] = c1;
+
+        }
+
+
+        // Search the pivoting element from each column
+
+        int k = 0;
+
+        for (int j = 0; j < n - 1; ++j) {
+
+            int pi1 = 0;
+
+            for (int i = j; i < n; ++i) {
+
+                int pi0 = Math.abs(a[index[i]][j]);
+
+                pi0 /= c[index[i]];
+
+                if (pi0 > pi1) {
+
+                    pi1 = pi0;
+
+                    k = i;
+
+                }
+
+            }
+
+
+            // Interchange rows according to the pivoting order
+
+            int itmp = index[j];
+
+            index[j] = index[k];
+
+            index[k] = itmp;
+
+            for (int i = j + 1; i < n; ++i) {
+
+                int pj = a[index[i]][j] / a[index[j]][j];
+
+
+                // Record pivoting ratios below the diagonal
+
+                a[index[i]][j] = pj;
+
+
+                // Modify other elements accordingly
+
+                for (int l = j + 1; l < n; ++l)
+
+                    a[index[i]][l] -= pj * a[index[j]][l];
+
+            }
+
+        }
+
+    }
 
     public String encrypt(String message) {
         int messageLength = message.length();
-        String key = generateRandomKey(messageLength);
-        double[][] keyMatrix = convertKeyToIntMatrix(key);
-        double[][] messageMatrix = convertMessageToIntMatrix(message);
-        multiplyMatrices(keyMatrix, messageMatrix);
+        String messageLowerCase = message.toLowerCase(Locale.ROOT);
+        key = generateRandomKey(messageLength);
+        keyMatrix = convertKeyToIntMatrix(key);
+        messageMatrix = convertMessageToIntMatrix(messageLowerCase);
+        encryptedKeyMatrix = multiplyMatrices(keyMatrix, messageMatrix);
+        encryptedKeyMatrix = mod26Matrix(encryptedKeyMatrix); // returning a matrix with zeros
+        encryptedMessage = convertMatrixToString(encryptedKeyMatrix);
 
 
         // convert key to a messageLength x messageLength array
@@ -37,11 +189,10 @@ public class HillCipherService {
         // multiply them
         // mod  26 the result
         // convert array to a char array
-        // return to user;
-        // INDArray matrixMultiply = eKey.mmul(eMessage);
+        // return to user
 
 
-        return multiplyMatrices(keyMatrix, messageMatrix);
+        return encryptedMessage;
     }
 
     public String encryptToNumber(String message) {
@@ -55,16 +206,30 @@ public class HillCipherService {
     }
 
     public int encryptCharToNumber(char letter) {
-        return letter - 'a' + 1;
+        return Integer.valueOf(letter - 'a' + 1);
     }
 
-    public char decryptNumberToChar(double number) {
-        return number > 0 && number < 27 ? Character.valueOf((char) ((int) number + 'a' - 1)) : null;
+    public char decryptNumberToChar(int number) {
+        return Character.valueOf((char) (number == 0 ? number + 1 : number + 'a' - 1));
     }
 
-    // inverse of the encrypted key matrix mod 26 * the encrypted message matrix
-    public String decrypt(String key, String message) {
-        return "I have a boyfriend!";
+    // inverse of the encrypted key matrix mod 26 * the encrypted message vector
+    public String decrypt(String message) {
+        // get inverse keyMatrix mod 26
+        // multiply by message
+        int[][] matrix = invert(keyMatrix);
+        matrix = mod26Matrix(matrix);
+        matrix = multiplyMatrices(matrix, encryptedKeyMatrix);
+
+        return convertMatrixToString(matrix);
+    }
+
+    public int[][] mod26Matrix(int[][] matrix) {
+        int[][] ans = new int[matrix.length][1];
+        for (int row = 0; row < matrix.length; row++) {
+            ans[row][0] = matrix[row][0] % 26;
+        }
+        return ans;
     }
 
     // generate random key based on message size EX: given message size is 3, key size is 3^2
@@ -73,15 +238,15 @@ public class HillCipherService {
         Random r = new Random();
         int keySize = (int) Math.pow(messageLength, 2);
         for (int i = 0; i < keySize; i++) {
-            sb.append(decryptNumberToChar(r.nextInt(26) + 1));
+            sb.append(decryptNumberToChar(r.nextInt(27 - 1) + 1));
         }
         return sb.toString();
     }
 
-    public double[][] convertKeyToIntMatrix(String key) {
-        int row = (int) Math.sqrt((double) key.length());
+    public int[][] convertKeyToIntMatrix(String key) {
+        int row = (int) Math.sqrt((int) key.length());
         int col = row;
-        double[][] keyMatrix = new double[row][col];
+        int[][] keyMatrix = new int[row][col];
         int countStringIndex = 0;
 
         for (int i = 0; i < keyMatrix.length; i++) {
@@ -94,8 +259,8 @@ public class HillCipherService {
         return keyMatrix;
     }
 
-    public double[][] convertMessageToIntMatrix(String message) {
-        double[][] matrix = new double[message.length()][1];
+    public int[][] convertMessageToIntMatrix(String message) {
+        int[][] matrix = new int[message.length()][1];
         for (int i = 0; i < matrix.length; i++) {
             matrix[i][0] = encryptCharToNumber(message.charAt(i));
         }
@@ -103,19 +268,32 @@ public class HillCipherService {
     }
 
     // multiply a 3x3 by 3x1
-    private String multiplyMatrices(double[][] keyMatrix, double[][] messageMatrix) {
+    // this method does two things which is not legal based on SOLID principles
+    // REFACTOR
+    private int[][] multiplyMatrices(int[][] keyMatrix, int[][] messageMatrix) {
         // multiply all col of every row by all rows and then sum, then mod 26 of sum
-        double[][] ans = new double[messageMatrix.length][1];
-        StringBuilder sb = new StringBuilder();
+        int[][] ans = new int[messageMatrix.length][1];
         for (int row = 0; row < keyMatrix.length; row++) {
             for (int col = 0; col < messageMatrix[0].length; col++) {
                 ans[row][0] += keyMatrix[row][col] * messageMatrix[row][0];
             }
-            ans[row][0] = ans[row][0] % 26;
-            sb.append(decryptNumberToChar(ans[row][0]));
         }
 
+        return ans;
+    }
 
+
+// Method to carry out the partial-pivoting Gaussian
+
+// elimination.  Here index[] stores pivoting order.
+
+    // x by 1 matrix
+    private String convertMatrixToString(int[][] matrix) {
+        StringBuilder sb = new StringBuilder();
+        for (int row = 0; row < matrix.length; row++) {
+            sb.append(decryptNumberToChar(matrix[row][0]));
+        }
         return sb.toString();
     }
+
 }
--- src/test/java/com/csdisciple/hill_cipher/HillCipherServiceTest.java
+++ src/test/java/com/csdisciple/hill_cipher/HillCipherServiceTest.java
@@ -1,6 +1,8 @@
 package com.csdisciple.hill_cipher;
 
 import org.junit.jupiter.api.Test;
+import org.la4j.Matrix;
+import org.la4j.matrix.dense.Basic2DMatrix;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -9,12 +11,15 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 public class HillCipherServiceTest {
-
+// tests are just too simplistic, only asserting length
+    // need to test for edge cases
+        // make sure we only work with lower case, non null, 1-26, a-z
+            // nothing else
     @Autowired
     private HillCipherService service;
 
     @Test
-    public void testDecrypt(){
+    public void testDecryptChar(){
         int encryptedChar = service.encryptCharToNumber('g');
 
         assertEquals('g', service.decryptNumberToChar(encryptedChar));
@@ -37,7 +42,7 @@ public class HillCipherServiceTest {
     public void convertKeyToIntMatrix(){
         int keyLength = 3;
         String randomKey = service.generateRandomKey(keyLength);
-        double[][] matrix = service.convertKeyToIntMatrix(randomKey);
+        int[][] matrix = service.convertKeyToIntMatrix(randomKey);
 
         assertEquals(keyLength, matrix.length);
 
@@ -47,9 +52,33 @@ public class HillCipherServiceTest {
     @Test
     public void convertMessageToIntMatrix(){
         int keyLength = 3;
+        // generates a key of length keyLength * keyLength
         String randomKey = service.generateRandomKey(keyLength);
-        double[][] matrix = service.convertMessageToIntMatrix(randomKey);
+        int[][] matrix = service.convertMessageToIntMatrix(randomKey);
 
-        assertEquals(keyLength*keyLength, matrix[0].length);
+        assertEquals(randomKey.length(), matrix.length);
     }
+
+    @Test
+    public void encrypt(){
+        String message = service.encrypt("hellomynameis");
+        assertEquals(message.length(), "hellomynameis".length());
+    }
+    @Test
+    public void decrypt(){
+        String message = service.decrypt("hellomynameis");
+        assertEquals(message.length(), "hellomynameis".length());
+    }
+//
+//    @Test
+//    public void mod26Matrix(){
+//        Matrix a = new Basic2DMatrix(new int[][]{
+//                { 100.0, 250.0, 39.0 },
+//                { 48.0, 54.0, 65.0 },
+//                { 77.0, 86.0, 95.0 }
+//        });
+//        String matrix = service.mod26Matrix(a).toString();
+//        System.out.println(matrix);
+//    }
+
 }
